0c5edf611a7a18f243253cefcd2de3696872e7e8
황중원 <jwhwang@amorepacific.com>
2019-08-20 14:17:12 +0000

    FEATURE baseline code

---
+++ src/train_emotion_kor_multi_modal_classifier.py
@@ -0,0 +1,82 @@
+# referenced from https://github.com/oarriaga/face_classification
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='0'
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow.keras as keras
+
+from models.cnn import mini_XCEPTION
+from utils.datasets import DataManager
+from utils.datasets import split_data
+from utils.preprocessor import preprocess_input
+
+# parameters
+#NUM_GPU = 6
+#batch_size = 32 * NUM_GPU
+batch_size = 32
+num_epochs = 1000
+input_shape = (48, 48, 1)
+validation_split = .2
+verbose = 1
+num_classes = 7
+patience = 500
+base_path = '../trained_models/kor_multi_modal_emotion_model_frontal_face_20190811/'
+print("train model path ", base_path)
+# mirrored_strategy = tf.distribute.MirroredStrategy()
+# with mirrored_strategy.scope():
+# data generator
+data_generator = ImageDataGenerator(
+                        featurewise_center=False,
+                        featurewise_std_normalization=False,
+                        rotation_range=10,
+                        width_shift_range=0.1,
+                        height_shift_range=0.1,
+                        zoom_range=.1,
+                        horizontal_flip=True)
+
+# model parameters/compilation
+model = mini_XCEPTION(input_shape, num_classes)
+# model = tf.keras.utils.multi_gpu_model(model, gpus=NUM_GPU)
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+
+
+
+datasets = ['kor_multi_modal']
+for dataset_name in datasets:
+    print('Training dataset:', dataset_name)
+
+    # callbacks
+    log_file_path = base_path + dataset_name + '_emotion_training.log'
+    csv_logger = CSVLogger(log_file_path, append=False)
+    early_stop = EarlyStopping('val_loss', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1,
+                                  patience=int(patience/4), verbose=1)
+    trained_models_path = base_path + dataset_name + '_mini_XCEPTION'
+    model_names = trained_models_path + '.{epoch:02d}-{val_accuracy:.2f}.hdf5'
+    model_checkpoint = ModelCheckpoint(model_names, 'val_loss', verbose=1,
+                                                    save_best_only=False)
+    callbacks = [model_checkpoint, csv_logger, early_stop, reduce_lr]
+
+    # loading dataset
+    data_loader = DataManager(dataset_name, image_size=input_shape[:2])
+    faces, emotions = data_loader.get_data()
+    faces = preprocess_input(faces)
+    num_samples, num_classes = emotions.shape
+    train_data, val_data = split_data(faces, emotions, validation_split)
+    train_faces, train_emotions = train_data
+    model.fit_generator(data_generator.flow(train_faces, train_emotions,
+                                            batch_size,
+                                            shuffle=True),
+                        steps_per_epoch=len(train_faces) / batch_size,
+                        epochs=num_epochs, verbose=1, callbacks=callbacks,
+                        validation_data=val_data,
+                        use_multiprocessing=True,
+			workers=6)
+
---
+++ src/utils/preprocessor.py
@@ -0,0 +1,29 @@
+import numpy as np
+# from scipy.misc.pilutil import imread, imresize
+from cv2 import resize
+import matplotlib.pyplot as plt
+
+
+def preprocess_input(x, v2=True):
+    x = x.astype('float32')
+    x = x / 255.0
+    if v2:
+        x = x - 0.5
+        x = x * 2.0
+    return x
+
+
+def _imread(image_name):
+        return plt.imread(image_name)
+
+
+def _imresize(image_array, size):
+        return resize(image_array, size)
+
+
+def to_categorical(integer_classes, num_classes=2):
+    integer_classes = np.asarray(integer_classes, dtype='int')
+    num_samples = integer_classes.shape[0]
+    categorical = np.zeros((num_samples, num_classes))
+    categorical[np.arange(num_samples), integer_classes] = 1
+    return categorical
